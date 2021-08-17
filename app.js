bd4744af6f975f0ed02cea2c0241880fe3ffbdae
zxfsgn <makarov011099@gmail.com>
2021-08-17 22:17:17 +0300

    main

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const cors = require('cors')
 const app = express()
 
 const PORT = process.env.PORT || config.get('port')
-
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(cors())
 app.use(express.json())
 
